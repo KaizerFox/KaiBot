b7a6192c1d60359a70fcba6259a752f31feb41f8
JakobTheFurry <29178963+JakobTheFurry@users.noreply.github.com>
2019-12-28 18:21:28 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -252,14 +252,15 @@ var after = `${replace}`;
 
   if(command === "uptime") {
 
-function duration(ms) {
-    const sec = Math.floor((ms / 1000) % 60).toString()
-    const min = Math.floor((ms / (1000 * 60)) % 60).toString()
-    const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString()
-    const days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 60).toString()
-    return `${days.padStart(1, '0')} days, ${hrs.padStart(2, '0')} hours, ${min.padStart(2, '0')} minutes, ${sec.padStart(2, '0')} seconds, `
+ type(message.channel,true,3);
+ async function duration(ms) {
+  const sec = Math.floor((ms / 1000) % 60).toString();
+  const min = Math.floor((ms / (1000 * 60)) % 60).toString();
+  const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString();
+  const days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 60).toString();
+  return await `${days.padStart(1, '0')} days, ${hrs.padStart(2, '0')} hours, ${min.padStart(2, '0')} minutes, ${sec.padStart(2, '0')} seconds, `;
 }
-
+ 
 message.channel.send(`I have been online for: ${duration(client.uptime)}`)
 }
 
