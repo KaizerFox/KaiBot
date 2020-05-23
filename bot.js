109ea11b78e0d0bfab0a60a5a5dd8f2f15f209c2
KaizerFox <29178963+KaizerFox@users.noreply.github.com>
2020-05-22 21:05:23 -0400

    added a randomstring command

--- bot.js
+++ bot.js
@@ -101,6 +101,16 @@ client.on("message", async message => {
        }
       }
 
+      if(command === "randomstring") {
+      var crypto = require("crypto");
+      var id = crypto.randomBytes(20).toString('hex');
+      var st = id.toString()
+      await type(message.channel,true,3);
+      await message.channel.send("" + st);
+      return await type(message.channel,false,0);
+      }
+      
+
       if (command === "bin") {
         const strx = args.join(" ");
         if (!strx) return;
