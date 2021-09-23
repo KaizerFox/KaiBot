978eb2f38dc96d2b8f6cea1584c952526ab81e4e
KaizerFox <29178963+KaizerFox@users.noreply.github.com>
2021-09-23 15:45:17 -0400

    Add files via upload

--- commands/sexflag.js
+++ commands/sexflag.js
@@ -1,12 +1,18 @@
+const path = require('path');
+
 module.exports = {
 	name: 'sexflag',
 	description: 'Turns any image into a sexuality!!!!! uwu',
 	args: true,
 	execute(message, args) { 
 		if (message.attachments.size > 0) {
-   
+			let request = require(`request`);
+			let fs = require('fs');
+			var imgSent = (message.attachments).array();
 
-    
+			   request.get(imgSent.proxyURL)
+            .on('error', console.error)
+            .pipe(fs.createWriteStream(`Img-${Date.now()}`));//The "Img-${Date.now}" Guarantees Unique file names.
     }  
 
 	},
