7c45744337b81dc88b7241ea3201e0a054169a08
sexflagfunny <91231588+sexflagfunny@users.noreply.github.com>
2021-09-24 13:44:42 -0400

    Update sexflag.js

--- commands/sexflag.js
+++ commands/sexflag.js
@@ -1,24 +1,16 @@
-
 const path = require('path');
-const fs = require('fs');
 
 module.exports = {
-	name: 'sexflag',
-	description: 'Turns any image into a sexuality!!!!! uwu',
-	args: true,
-	execute(message, args) { 
-		if (message.attachments.size > 0) {
-			if (message.attachments.every(attachIsImage)){
-				fs.writeFileSync(`./${a.name}`, a.file)
+    name: 'sexflag',
+    description: 'Turns any image into a sexuality!!!!! uwu',
+    args: true,
+    execute(message, args) { 
+        if (message.attachments.size === 1) {
+            if(extension == "png" || extension == "jpg" || extension == "jpeg" || extension == "webp") {
+				var a = message.attachment
+                fs.writeFileSync(`./${a.name}`, a.file)
 			}
-		}
-	},
+        }
+    },
 };
-
-
-function attachIsImage(msgAttach) {
-    var url = msgAttach.url;
-    //True if this url is a png image.
-    return url.indexOf("png", url.length - "png".length /*or 3*/) !== -1;
-}
 
