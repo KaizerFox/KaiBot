530d2bc2027dc875cccf1f950ec379695be71bc1
KaizerFox <29178963+KaizerFox@users.noreply.github.com>
2022-08-14 10:08:18 -0400

    Add files via upload

--- commands/kick.js
+++ commands/kick.js
@@ -6,7 +6,7 @@ module.exports = {
 			return message.reply('you need to tag a user in order to kick them!');
 		}
 
-		if (message.author.guild_permissions.administrator == true) {
+		if (message.author.hasPermission("ADMINISTRATOR") == true) {
 		const taggedUser = message.mentions.users.first();
 
 		message.channel.send(`You wanted to kick: ${taggedUser.username}`);
